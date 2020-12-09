b3f3da88520913545f1c2e1fcadad23eb6e061f3
jaeyoungchang5 <58242673+jaeyoungchang5@users.noreply.github.com>
2020-12-09 12:03:27 -0500

    frontend: minimal react bootstrap added to home, login, signup pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import userUtil from '../utils/user.util';
+import {Button, Form, Jumbotron} from 'react-bootstrap';
 
 function Login() {
     const [user, setUser] = useState({
@@ -29,12 +30,24 @@ function Login() {
 
     return (
         <div>
-            <h1>Login Page</h1>
-            <form>
-                <input type='text' value={user.username} placeholder='Username' name='username' onChange={handleChange} autoComplete='off' />
-                <input type='password' value={user.password} placeholder='Password' name='password' onChange={handleChange} autoComplete='off' />
-                <button onClick={handleSubmit}>Log In</button>
-            </form>
+            <Jumbotron>
+                <h1>Login Page</h1>
+            </Jumbotron>
+            <Form>
+                <Form.Group controlId="username">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type='text' value={user.username} placeholder='Username' name='username' onChange={handleChange} autoComplete='off' />
+                </Form.Group>
+
+                <Form.Group controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type='password' value={user.password} placeholder='Password' name='password' onChange={handleChange} autoComplete='off' />
+                </Form.Group>
+                
+                <Button onClick={handleSubmit} variant="primary" type="submit">
+                    Log In
+                </Button>
+            </Form>
         </div>
     );
 }
