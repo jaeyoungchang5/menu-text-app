f0cd3363a85c637d625e8ecb652c79db0b477ba9
jaeyoungchang5 <58242673+jaeyoungchang5@users.noreply.github.com>
2020-12-09 00:32:43 -0500

    backend: able to make login and signup calls to database

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,42 @@
+const User = require('../models/user.model');
+
+module.exports = {
+    signup,
+    login
+};
+
+async function signup(req, res){
+    console.log('Backend: Signing up');
+    const user = new User(req.body);
+    try {
+        await user.save();
+        res.json({status: 'Success'});
+    } catch (err) {
+        res.status(400).json(err);
+    }
+}
+
+async function login(req, res){
+    console.log('Backend: Logging in');
+    try {
+        const user = await User.findOne({username: req.body.username});
+
+        if (!user){
+            res.status(401).json({err: 'Bad credentials'});
+            return;
+        }
+
+        if (req.body.password === user.password){
+            console.log('Found user: Correct password');
+            res.json({status: 'Success'});
+        } else {
+            console.log('Found user: Incorrect password');
+            res.json({status: 'Error', message: 'Incorrect password'});
+        }
+
+
+    } catch (err) {
+        res.status(401).json(err);
+        return;
+    } 
+}
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 const backend_port = 5000;
 
+app.use(express.json());
+
+const routes = require('./routes/router');
+
+require('./config/user.database');
+app.use('/api', routes);
+
 app.listen(backend_port, () => {
     console.log('Backend: Server is listening on port ' + backend_port)
-});
-
-app.get('/api', (req, res) => {
-    console.log('Backend: connection is working')
-    res.send({express: 'backend is connected'});
 });
