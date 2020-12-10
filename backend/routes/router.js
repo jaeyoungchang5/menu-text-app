1d17d56a9c8b6d918bcca3a8b1ba6cd527119458
jaeyoungchang5 <58242673+jaeyoungchang5@users.noreply.github.com>
2020-12-10 12:28:32 -0500

    backend: set up menu routes, model, and get/post requests

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const menusCtrl = require('../controllers/menu.controller');
 const usersCtrl = require('../controllers/user.controller');
 
 /*---------- Public Routes ----------*/
@@ -7,5 +8,11 @@ const usersCtrl = require('../controllers/user.controller');
 /* user */
 router.post('/signup', usersCtrl.signup);
 router.post('/login', usersCtrl.login);
+
+/* menu */
+router.get('/menu/:date/:meal/:diningHall', menusCtrl.getMenu);
+router.post('/menu/', menusCtrl.postMenu);
+router.put('/menu/:date/:meal', menusCtrl.putMenu);
+router.delete('/menu/:date/:meal', menusCtrl.deleteMenu);
 
 module.exports = router;
