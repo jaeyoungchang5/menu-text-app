872afd62f7361482c5aa59260ff36f5307d2a4db
jaeyoungchang5 <58242673+jaeyoungchang5@users.noreply.github.com>
2020-12-10 12:24:48 -0500

    backend: router linting

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 const usersCtrl = require('../controllers/user.controller');
 
 /*---------- Public Routes ----------*/
 
 /* user */
 router.post('/signup', usersCtrl.signup);
-router.post('/login', (usersCtrl.login));
+router.post('/login', usersCtrl.login);
 
 module.exports = router;
