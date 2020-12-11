35b482833ca3ca310756a173f3c0e2880006dd11
jaeyoungchang5 <58242673+jaeyoungchang5@users.noreply.github.com>
2020-12-11 09:40:49 -0500

    backend: modified signup and login functions, no longer async func

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -6,37 +6,34 @@ module.exports = {
     login
 };
 
-async function signup(req, res){
+function signup(req, res){
     console.log('USER.CONTROLLER: Signing up');
     const user = new User(req.body);
-    try {
-        console.log('USER.CONTROLLER: signup succes');
-        await user.save();
-
-        const token = createToken(user);
+    user.save()
+    .then(newUser => {
+        const token = createToken(newUser);
         res.header('auth-token', token);
         res.status(201).json({result: 'success', message: 'Signup successful', token: token});
-    } catch (err) {
+    }).catch(err => {
         console.log('USER.CONTROLLER: signup error');
         console.log(err);
         res.status(400).json(err);
-    }
+    });   
 }
 
-async function login(req, res){
+function login(req, res){
     console.log('USER.CONTROLLER: Logging in');
-    try {
-        const user = await User.findOne({username: req.body.username});
-
-        if (!user){
+    User.findOne({username: req.body.username})
+    .then(foundUser => {
+        if (!foundUser){
             res.status(404).json({result: 'error', message: 'Username not found'});
             return;
         }
 
-        user.checkPassword(req.body.password, (err,result) => {
+        foundUser.checkPassword(req.body.password, (err,result) => {
             if (result){
                 console.log('Found user: Correct password');
-                const token = createToken(user);
+                const token = createToken(foundUser);
                 res.header('auth-token', token);
                 res.status(200).json({result: 'success', message: 'Login successful', token: token});
             } else {
@@ -44,14 +41,12 @@ async function login(req, res){
                 res.status(400).json({result: 'error', message: 'Incorrect password'});
             }
         });
-
-
-    } catch (err) {
+    }).catch(err => {
         console.log('weird error');
         console.log(err);
         res.status(401).json(err);
         return;
-    } 
+    });
 }
 
 function createToken(user) {
